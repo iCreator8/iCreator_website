064828a7b63684b097e05b9a8283fce08e14da1d
tiger-86 <iselfchallenge@gmail.com>
2022-11-06 23:16:35 -0800

    Home page mobile layout well in progress - portrait

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+// Make the screen to fit into viewport wile URL tab is down or up 
 
+// We listen to the resize event
+window.addEventListener('resize', () => {
 
-// Mobile Menu Toggle 
-document.querySelector('div input').onclick = function() {
-    if(this.checked)
-      this.setAttribute("aria-expanded",true)
-    else
-      this.setAttribute("aria-expanded",false)
-}
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+
+  let vh = window.innerHeight * 0.01;
+
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
